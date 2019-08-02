b3ccc1e80671b51cbf9d6791fb053fe3f31cc596
shubham chauhan <shubham7266@gmail.com>
2019-08-02 15:53:02 +0530

    initial commit

--- app/src/main/java/com/event/reminder/ui/ViewModelFactory.kt
+++ app/src/main/java/com/event/reminder/ui/ViewModelFactory.kt
@@ -2,10 +2,12 @@ package com.event.reminder.ui
 
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
-import com.event.reminder.data.repository.LoginRepository
 import com.event.reminder.data.repository.HomeRepository
+import com.event.reminder.data.repository.LoginRepository
+import com.event.reminder.data.repository.SignUpRepository
 import com.event.reminder.ui.dashboard.HomeViewModel
 import com.event.reminder.ui.login.LoginViewModel
+import com.event.reminder.ui.login.SignUpViewModel
 
 /**
  * ViewModel provider factory to instantiate ViewModel.
@@ -19,6 +21,9 @@ class ViewModelFactory : ViewModelProvider.Factory {
         return when {
             modelClass.isAssignableFrom(LoginViewModel::class.java) -> LoginViewModel(
                 loginRepository = LoginRepository
+            ) as T
+            modelClass.isAssignableFrom(SignUpViewModel::class.java) -> SignUpViewModel(
+                signUpRepository = SignUpRepository
             ) as T
             modelClass.isAssignableFrom(HomeViewModel::class.java) -> HomeViewModel(
                 homeRepository = HomeRepository
--- app/src/main/java/com/event/reminder/ui/login/SignUpViewModel.kt
+++ app/src/main/java/com/event/reminder/ui/login/SignUpViewModel.kt
@@ -1,6 +1,62 @@
 package com.event.reminder.ui.login
 
+import android.databinding.Bindable
 import com.android.mvvmandroidlib.viewmodel.BaseObservableViewModel
+import com.event.reminder.BR
+import com.event.reminder.data.repository.SignUpRepository
 
-class SignUpViewModel : BaseObservableViewModel() {
+class SignUpViewModel(private val signUpRepository: SignUpRepository) : BaseObservableViewModel() {
+
+    var fullName: String? = null
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.fullName)
+        }
+
+    var emailId: String? = null
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.emailId)
+        }
+
+    var dateOfBirth: String? = null
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.dateOfBirth)
+        }
+
+    var gender: String? = null
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.gender)
+        }
+
+    var phoneNumber: String? = null
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.phoneNumber)
+        }
+
+    var password: String? = null
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.password)
+        }
+
+    var confirmPassword: String? = null
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.confirmPassword)
+        }
+
+    fun signUp() {
+
+    }
 }
