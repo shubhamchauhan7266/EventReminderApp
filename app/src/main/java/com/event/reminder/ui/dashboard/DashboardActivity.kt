a633fd069985a5d471dbf1c918268891a3bebc12
shubham chauhan <shubham7266@gmail.com>
2019-08-21 15:17:38 +0530

    initial commit

--- app/src/main/java/com/event/reminder/ui/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/event/reminder/ui/dashboard/DashboardActivity.kt
@@ -3,6 +3,7 @@ package com.event.reminder.ui.dashboard
 import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.support.design.widget.BottomNavigationView
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
@@ -13,13 +14,14 @@ import com.event.reminder.ui.ViewModelFactory
 
 
 class DashboardActivity : BaseActivity<DashboardActivityBinding, DashboardViewModel>() {
+
+    private lateinit var navController: NavController
+
     override fun onCreateBinding() {
         binding.viewModel = viewModel
+        navController = Navigation.findNavController(this@DashboardActivity, R.id.nav_dashboard_fragment)
         binding.navView.setupWithNavController(
-            Navigation.findNavController(
-                this@DashboardActivity,
-                R.id.nav_dashboard_fragment
-            )
+            navController
         )
     }
 
@@ -44,7 +46,11 @@ class DashboardActivity : BaseActivity<DashboardActivityBinding, DashboardViewMo
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         onNavDestinationSelected(
             item,
-            Navigation.findNavController(this@DashboardActivity, R.id.nav_dashboard_fragment)
+            navController
         )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
