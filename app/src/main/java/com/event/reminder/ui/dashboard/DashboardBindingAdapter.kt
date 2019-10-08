726b66f977f27e05c06d19ffe4341bc931439fce
Shubham Chauhan <shubahm7266@gmail.com>
2019-10-08 14:57:13 +0530

    initial commit

--- app/src/main/java/com/event/reminder/ui/dashboard/DashboardBindingAdapter.kt
+++ app/src/main/java/com/event/reminder/ui/dashboard/DashboardBindingAdapter.kt
@@ -1,2 +1,28 @@
 package com.event.reminder.ui.dashboard
 
+import android.databinding.BindingAdapter
+import android.graphics.drawable.Drawable
+import android.widget.ImageView
+import android.widget.TextView
+import com.android.mvvmandroidlib.utills.DateUtils
+import com.android.mvvmandroidlib.utills.StringUtils
+import com.event.reminder.EventReminderApplication
+import com.squareup.picasso.Picasso
+
+@BindingAdapter(value = ["imageUrl", "placeholder"], requireAll = false)
+fun loadImage(imageView: ImageView, url: String?, placeHolder: Drawable?) {
+    if (StringUtils.isNullOrEmpty(url)) {
+        imageView.setImageDrawable(placeHolder)
+    } else {
+        Picasso.with(EventReminderApplication.getInstance().applicationContext)
+            .load(url)
+            .error(placeHolder)
+            .into(imageView)
+    }
+}
+
+@BindingAdapter("timeStamp")
+fun setDate(view: TextView, timeStamp: Long?) {
+    view.text = DateUtils.formatDate(timeStamp, DateUtils.HH_MM_AA)
+}
+
