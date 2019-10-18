673ab3f72338e2bac2f4490bb09d3f51c03fd34c
Shubham Chauhan <shubahm7266@gmail.com>
2019-10-18 07:52:24 +0530

    initial commit

--- app/src/main/java/com/event/reminder/ui/EventReminderBindingAdapter.kt
+++ app/src/main/java/com/event/reminder/ui/EventReminderBindingAdapter.kt
@@ -5,20 +5,41 @@ import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import android.widget.TextView
 import com.android.mvvmandroidlib.exception.DateUtilParseException
+import com.android.mvvmandroidlib.utills.ContextUtils
 import com.android.mvvmandroidlib.utills.DateUtils
+import com.android.mvvmandroidlib.utills.DisplayUtils
 import com.android.mvvmandroidlib.utills.StringUtils
-import com.event.reminder.EventReminderApplication
 import com.squareup.picasso.Picasso
 
-@BindingAdapter(value = ["bind:imageUrl", "bind:placeholder"], requireAll = false)
-fun loadImage(imageView: ImageView, url: String?, placeHolder: Drawable?) {
+@BindingAdapter(
+    value = ["bind:imageUrl", "bind:placeholder", "android:layout_width", "android:layout_height"],
+    requireAll = false
+)
+fun loadImage(
+    imageView: ImageView,
+    url: String?,
+    placeHolder: Drawable?,
+    width: Float,
+    height: Float
+) {
     if (StringUtils.isNullOrEmpty(url)) {
         imageView.setImageDrawable(placeHolder)
     } else {
-        Picasso.with(EventReminderApplication.getInstance().applicationContext)
-            .load(url)
-            .error(placeHolder)
-            .into(imageView)
+        try {
+            Picasso.with(imageView.context)
+                .load(url)
+                .placeholder(placeHolder)
+                .error(placeHolder)
+                .resize(
+                    DisplayUtils.convertDpToPixel(width, imageView.context).toInt(),
+                    DisplayUtils.convertDpToPixel(height, imageView.context).toInt()
+                )
+                .centerCrop()
+                .onlyScaleDown()
+                .into(imageView)
+        } catch (e: Exception) {
+            imageView.setImageDrawable(placeHolder)
+        }
     }
 }
 
@@ -26,8 +47,8 @@ fun loadImage(imageView: ImageView, url: String?, placeHolder: Drawable?) {
 fun setDate(view: TextView, timeStamp: Long?, timeFormat: String?) {
     try {
         view.text = DateUtils.formatDate(timeStamp, timeFormat)
-    }catch (e: DateUtilParseException){
-        view.text = ""
+    } catch (e: DateUtilParseException) {
+        view.text = StringUtils.EMPTY
     }
 }
 
